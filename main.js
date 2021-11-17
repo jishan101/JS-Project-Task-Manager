21f1202e63d69825123148de48fa6a62c4342203
jishan101 <rko.jishan@gmail.com>
2021-11-17 21:21:24 +0600

    db

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// import "regenerator-runtime/runtime"
+import "regenerator-runtime/runtime"
 
 
 
-async function del() {
-    const response = await fetch(`https://task-manager-server-app.herokuapp.com/`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify(
-            ongoing = [],
-            completed = []
-        )
-    });
+// async function del() {
+//     const response = await fetch(`https://task-manager-server-app.herokuapp.com/ongoing/1`, {
+//         method: "DELETE"
+//         // headers: {
+//         //     "Content-type": "application/json"
+//         // },
+//         // body: JSON.stringify({
+//         //     id: 2,
+//         //     name: "Jishan",
+//         //     age: 25
+//         // })
+//     });
 
-    const data = await response.json();
-    console.log(data);
-}
+//     const data = await response.json();
+//     console.log(data);
+// }
 
 del();
