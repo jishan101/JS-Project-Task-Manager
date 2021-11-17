e67d2ff81c1f3dc05f51c22e49f6780731d6451e
jishan101 <rko.jishan@gmail.com>
2021-11-17 20:42:33 +0600

    changes

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 import "regenerator-runtime/runtime"
 
+
+
+async function del() {
+    const response = await fetch(`https://task-manager-server-app.herokuapp.com/users`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "id": 9,
+            "first_name": "Rania",
+            "last_name": "Dablin",
+            "email": "rdablin8@state.gov",
+            "gender": "Female"
+        })
+    });
+
+    const data = await response.json();
+    console.log(data);
+}
+
+del();
